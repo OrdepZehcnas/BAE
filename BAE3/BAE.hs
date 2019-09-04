e524c95beaeaa870423d1c41ebc3c7678e047459
OrdepZehcnas <pedro_merolito@hotmail.com>
2019-09-03 22:13:27 -0500

    Actualizando el proyecto

--- BAE3/BAE.hs
+++ BAE3/BAE.hs
@@ -39,6 +39,24 @@ module BAE where
     show (Ift e1 e2 e3) = "if("++show e1++","++show e2++","++show e3++")"
     show (LetE x e1 e2) = "let("++show e1++","++x++"."++show e2++")"
 
+-- | transform. funcion que dada una declaracion devuelve su expresion Aritmetico-Booleana correspondiente.
+  transform :: Stmt -> BAE
+  transform (Var n) = V n
+  transform (Num x) = N (fromIntegral x)
+  transform (BoolCt b) = B b
+  transform (ABUnary Not e) = Neg (transform e)
+  transform (ABUnary Succ e) = Suc (transform e)
+  transform (ABUnary Pred e) = Pre (transform e)
+  transform (ABBinary And e1 e2) = Conj (transform e1) (transform e2)
+  transform (ABBinary Or e1 e2) = Disy (transform e1) (transform e2)
+  transform (ABBinary Add e1 e2) = Plus (transform e1) (transform e2)
+  transform (ABBinary Times e1 e2) = Prod (transform e1) (transform e2)
+  transform (ABRel GrT e1 e2) = Gt (transform e1) (transform e2)
+  transform (ABRel LowT e1 e2) = Lt (transform e1) (transform e2)
+  transform (ABRel Equ e1 e2) = Equi (transform e1) (transform e2)
+  transform (If e1 e2 e3) = Ift (transform e1) (transform e2) (transform e3)
+  transform (Let x e1 e2) = LetE x (transform e1) (transform e2)    
+
 -- | fv. funcion que devuelve el conjunto de variables libres de una expresion Aritmetico-Booleanas.
   fv :: BAE -> [Name]
   fv e = error "error"
